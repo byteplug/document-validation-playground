74aeda24d847f2290294a70efb18829eaedc5608
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2022-06-12 02:28:18 +0700

    Installed axios and setup project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
+import axios from 'axios'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import { Inkline, components } from '@inkline/inkline'
-import '@inkline/inkline/inkline.scss'
 
+import '@inkline/inkline/inkline.scss'
 import "./main.scss"
 
 const app = createApp(App)
-
 
 app.use(Inkline, {
     components,
     colorMode: 'light'
 })
+
+axios.defaults.baseURL = 'https://api.byteplug.io'
+app.config.globalProperties.axios = axios
 
 app.mount('#app')
