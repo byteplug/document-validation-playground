990e95e68e4a3b981ac1fe8516a07a5165d43a36
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2022-06-12 02:24:17 +0700

    Reworked layout of code base

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 import { Inkline, components } from '@inkline/inkline'
 import '@inkline/inkline/inkline.scss'
@@ -9,7 +8,6 @@ import "./main.scss"
 
 const app = createApp(App)
 
-app.use(router)
 
 app.use(Inkline, {
     components,
